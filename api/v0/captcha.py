d054bd3eeb3c08d2a1e18b6a95d6bafd40a5a516
zau <zausang@163.com>
2020-03-08 19:32:26 +0800

    验证码识别api

--- api/v0/captcha.py
+++ api/v0/captcha.py
@@ -1,8 +1,20 @@
+import io
+
+from PIL import Image
 from flask import request
 
+from library.cnn_captcha.cnnlib.recognition_object import Recognizer
+from library.cnn_captcha.settings import CAPTCHA_CONFIG
 from library.redprint import RedPrint
 
 api = RedPrint("captcha")
+image_height = CAPTCHA_CONFIG["image_height"]
+image_width = CAPTCHA_CONFIG["image_width"]
+max_captcha = CAPTCHA_CONFIG["max_captcha"]
+char_set = CAPTCHA_CONFIG["char_set"]
+model_save_dir = CAPTCHA_CONFIG["model_save_dir"]
+model_save_name = "hb56.ckpt" or CAPTCHA_CONFIG["model_save_name"]
+rec = Recognizer(image_height, image_width, max_captcha, char_set, model_save_dir, model_save_name)
 
 
 @api.route("/", methods=["GET"])
@@ -20,4 +32,12 @@ def read_captcha():
     识别或上传一张验证码图片
     :return:
     """
+    # 暂定：图片字段 img
+    img = request.files.get("img")
+    if img:
+        img_bytes = io.BytesIO(img.stream.read())
+        r_img = Image.open(img_bytes)
+        r_img = r_img.resize((image_width, image_height))
+        text = rec.rec_image(r_img)
+        return text
     return f"Data:{request.args}"
