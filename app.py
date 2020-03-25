f723478fe495537a8c0aa4732000d165ac101792
jayzau <zausang@163.com>
2020-03-25 17:13:39 +0800

    异常捕获

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 
-from api.v0 import create_blueprint
 from config.settings import REDIS_URL
-from library.async_rq2 import rq
 
 
 def register_blueprints(app):
+    from api.v0 import create_blueprint
+
     app.register_blueprint(create_blueprint(), url_prefix="/api_v0")
 
 
 def register_rq(app):
+    from library.async_rq2 import rq
+
     app.config['RQ_REDIS_URL'] = REDIS_URL
     rq.init_app(app)
     """
@@ -19,10 +21,32 @@ def register_rq(app):
     return app
 
 
+def err_handel(app):
+    from werkzeug.exceptions import HTTPException
+    from library.exceptions import ApiException
+
+    @app.errorhandler(Exception)
+    def framework_error(e):
+        if isinstance(e, ApiException):
+            return e
+        if isinstance(e, HTTPException):
+            code = e.code
+            msg = e.description
+            error_code = 1000
+            return ApiException(code=code, msg=msg, error_code=error_code)
+        else:
+            if not app.config['DEBUG']:
+                return ApiException()
+            raise e
+    return app
+
+
 def create_app():
     app = Flask(__name__)
     # 蓝图注册
     register_blueprints(app)
+    # 异常处理
+    app = err_handel(app)
     # rq2异步任务注册
     app = register_rq(app)
     return app
